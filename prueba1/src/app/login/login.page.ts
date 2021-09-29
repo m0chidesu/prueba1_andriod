0645fc529731aa93b1510e4c8415e95d9a7274d4
m0chidesu <ctorreslazo@hotmail.com>
2021-09-28 22:40:27 -0300

    COMPLETACION DE LOGIN

--- prueba1/src/app/login/login.page.ts
+++ prueba1/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
+  login(form){
+    var user = form.value["name"]
+    var pass = form.value["password"]
 
+    console.log(form.value) //para testeo (unused)
+    if(user == "admin" && pass == "123"){
+      //redirect a productos
+      this.router.navigate(['/productos'])
+    }else{
+      console.log('Error')
+    }
+  }
 }
