142670d8e7622349c7349a400d293b9a4da5a412
m0chidesu <ctorreslazo@hotmail.com>
2021-09-28 22:12:24 -0300

    COMPLETADO DE SERVICIO.TS

--- prueba1/src/app/productos/service.service.ts
+++ prueba1/src/app/productos/service.service.ts
@@ -8,14 +8,26 @@ export class ServiceService {
   private productos = [{
     id : '1',
     nombre : 'Marraqueta',
-    imagenURL : "",
+    imagenURL : "https://t2.rg.ltmcdn.com/es/images/6/6/7/img_marraquetas_chilenas_11766_orig.jpg",
     comentarios : ['Marraqueta, crujiente xD']
   },
   {
     id : '2',
-    nombre : 'Pastelito',
-    imagenURL : "",
-    comentarios : ""
+    nombre : 'Pastel Selva Negra',
+    imagenURL : "https://d1kxxrc2vqy8oa.cloudfront.net/wp-content/uploads/2021/01/18204057/RFB-1910-1-tortadeselvanegra.jpg",
+    comentarios : ['Un pastel para los más golosos, Hecha con ingredientes naturales']
+  },
+  {
+    id: '3',
+    nombre : 'Pie de Limon',
+    imagenURL : "https://www.elmundoeats.com/wp-content/uploads/2017/10/Lemon-Meringue-Pie-2.jpg",
+    comentarios : ['Una tarta con relleno de limón dulce y agrio, Apto para reconciliaciones']
+  },
+  {
+    id : '4',
+    nombre : 'Dobladitas',
+    imagenURL : 'https://img-global.cpcdn.com/recipes/be91c61e0a03e7f3/1200x630cq70/photo.jpg',
+    comentarios : ['¿Que mejor que una doblada con mantequilla?, Nah, el dueño lo prefiere con queso :3'] 
   }
 ]
 
