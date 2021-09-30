6f8e62e5f0aa931c8be3042e02de48354d5b04e2
m0chidesu <ctorreslazo@hotmail.com>
2021-09-30 15:06:00 -0300

    ERROR EN AGREGAR

--- prueba1/src/app/app-routing.module.ts
+++ prueba1/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
 const routes: Routes = [
   {
     path: '',
@@ -29,8 +28,9 @@ const routes: Routes = [
     path : "login",
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   }
-
 ];
+ 
+
 
 @NgModule({
   imports: [
