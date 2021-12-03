cc55d63c4f31b2bf0982c6c582d1ef3ef065ebc1
m0chidesu <ctorreslazo@hotmail.com>
2021-12-02 22:52:53 -0300

    AGREGADO DE ERRORES

--- prueba1/src/app/login/login.page.ts
+++ prueba1/src/app/login/login.page.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { async } from '@angular/core/testing';
 import { Router } from '@angular/router';
@@ -15,18 +16,16 @@ export class LoginPage implements OnInit {
   private usuarios : any = [];
   private listafor : any= [];
   
-  constructor(private router : Router, private alertController : AlertController,private servicio :  ServiceService) { }
+  constructor(private router : Router, private alertController : AlertController,private servicio :  ServiceService
+    ) { }
 
   ngOnInit() {
-    this.servicio.getUsers().subscribe(
-      (resp) =>{ this.usuarios = resp},
-      (error) =>{console.log(error)}
-    );
 
   }
   async login(form){
     var username = form.value["name"]
     var password = form.value["password"]
+    if(username != '' && password != ''){
 
       const axios = require('axios')
       axios.post('http://localhost:1337/auth/local',{
@@ -39,13 +38,38 @@ export class LoginPage implements OnInit {
         console.log('User profile', response.data.user);
         
         this.router.navigate(['/productos'])
-        
       })
       .catch(error =>{
         console.log('Error : ', error.response)
         
       });
 
+    }else if(username == '' && password == '' ){
+        const alert = await this.alertController.create({
+          cssClass: 'my-custom-class',
+          header: 'Error',
+          message: 'Complete los datos',
+          buttons: ['OK']
+        });
+    
+        await alert.present();
+    
+        const { role } = await alert.onDidDismiss();
+      
+    }else if(form.value["name"] != username && form.value["password"] != password){
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'Error',
+        message: 'Usuario o contraseña incorrecta',
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+  
+      const { role } = await alert.onDidDismiss();
+      
+    }
+     
   }
   redirectRegister(){
       console.log('redirect pass')
